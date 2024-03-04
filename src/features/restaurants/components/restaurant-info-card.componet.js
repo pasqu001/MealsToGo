5fc7145410777e43e31514d625dec290c168e312
Kasey Pasqualini <kaseypasqualini@yahoo.com>
2024-03-04 15:27:34 -0500

    finished card component

--- src/features/restaurants/components/restaurant-info-card.componet.js
+++ src/features/restaurants/components/restaurant-info-card.componet.js
@@ -1,56 +1,30 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from "react";
 import styled from "styled-components/native";
-import { Text, Image, View } from "react-native";
+import { Image, View } from "react-native";
 import { Card } from "react-native-paper";
 import { SvgXml } from "react-native-svg";
 
+import { Text } from "../../../components/typography/text.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
 
-const RestuarantCard = styled(Card)`
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-const RestaurantCardCover = styled(Card.Cover)`
-  padding: ${(props) => props.theme.space[3]};
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-const Address = styled(Text)`
-  font-family: ${(props) => props.theme.fonts.body};
-  font-size: ${(props) => props.theme.fontSizes.caption};
-`;
-const Title = styled(Text)`
-  font-family: ${(props) => props.theme.fonts.heading};
-  font-size: ${(props) => props.theme.fontSizes.body};
-  color: ${(props) => props.theme.colors.text.primary};
-`;
-const Info = styled(View)`
-  padding: ${(props) => props.theme.space[3]};
-`;
-const Rating = styled(View)`
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  padding-top: ${(props) => props.theme.space[2]};
-  padding-bottom: ${(props) => props.theme.space[2]};
-`;
-const Section = styled.View`
-  flex-direction: row;
-  align-items: center;
-`;
-const SectionEnd = styled.View`
-  flex: 1;
-  flex-direction: row;
-  justify-content: flex-end;
-`;
-const ClosedTemporarilyText = styled.View`
-  margin-right: ${(props) => props.theme.space[3]};
-`;
+import {
+  RestuarantCard,
+  RestaurantCardCover,
+  Info,
+  Section,
+  SectionEnd,
+  Rating,
+  Icon,
+  Address,
+  ClosedTemporarilyText,
+} from "./restaurant-info-card.styles";
 
 export const RestuarantInfoCard = ({ restaurant = {} }) => {
   const {
-    name = "Hummus Pear Dip",
+    name = "Hummus Bean Dip",
     icon = open,
     photos = ["https://picsum.photos/700"],
     address = "100 some random Street New York NY",
@@ -65,7 +39,7 @@ export const RestuarantInfoCard = ({ restaurant = {} }) => {
     <RestuarantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Info>
-        <Title>{name}</Title>
+        <Text variant="label">{name}</Text>
         <Section>
           <Rating>
             {ratingArray.map(() => (
@@ -75,16 +49,14 @@ export const RestuarantInfoCard = ({ restaurant = {} }) => {
           <SectionEnd>
             <ClosedTemporarilyText>
               {isClosedTemporarily && (
-                <Text variant="label" style={{ color: "red" }}>
-                  CLOSED TEMPORARILY
-                </Text>
+                <Text variant="error">CLOSED TEMPORARILY</Text>
               )}
             </ClosedTemporarilyText>
             <Spacer position="left" size="large">
               {isOpenNow && <SvgXml xml={icon} width={20} height={20} />}
             </Spacer>
             <Spacer position="left" size="large">
-              <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+              <Icon source={{ uri: icon }} />
             </Spacer>
           </SectionEnd>
         </Section>
